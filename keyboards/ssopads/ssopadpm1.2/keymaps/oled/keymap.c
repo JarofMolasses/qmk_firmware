0cbf0aa5ef630bb61dcbddbdae6f10bdb94f05fc
JarofMolasses <33560291+JarofMolasses@users.noreply.github.com>
2021-01-06 10:48:21 -0800

    rename layers

--- keyboards/ssopads/ssopadpm1.2/keymaps/oled/keymap.c
+++ keyboards/ssopads/ssopadpm1.2/keymaps/oled/keymap.c
@@ -25,7 +25,7 @@ void oled_print_static_aliases(uint8_t);
 void oled_print_keymap_aliases(uint8_t);
 void clearStr(char[]);
 
-enum ssopad_layers { _BASE, _FUNC, _FUNC2 };
+enum keymap_layers { _BASE, _NUM, _FUNC };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(/* BASE */
@@ -33,19 +33,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_HOME, KC_UP, KC_PGUP, KC_VOLU,    \
         KC_LEFT, KC_DOWN, KC_RIGHT, KC_VOLD, \
         KC_END, KC_NO, KC_PGDN, KC_LGUI,     \
-        KC_INS, TG(_FUNC), KC_DEL, MO(_FUNC2)     \
+        KC_INS, TG(_NUM), KC_DEL, MO(_FUNC)     \
      ),
 
 
-    [_FUNC] = LAYOUT(/* NUM */
+    [_NUM] = LAYOUT(/* NUM */
         KC_BSPC, KC_PSLS,\
         KC_7, KC_8, KC_9, KC_PAST,\
         KC_4, KC_5, KC_6, KC_PMNS,\
         KC_1, KC_2, KC_3, KC_PPLS,\
-        KC_0, KC_TRNS, KC_PDOT, LT(_FUNC2, KC_PENT)\
+        KC_0, KC_TRNS, KC_PDOT, LT(_FUNC, KC_PENT)\
     ),
 
-    [_FUNC2] = LAYOUT(/* function */
+    [_FUNC] = LAYOUT(/* function */
         KC_BSPC, KC_NO,\
         KC_F7, KC_F8, KC_F9, BL_INC,\
         KC_F4, KC_F5, KC_F6, BL_DEC,\
@@ -68,24 +68,24 @@ const char * static_aliases[][MATRIX_ROWS][MATRIX_COLS] = {
         {"    ", "NUM ", "     ", "FN   "}
     },
 
-    [_FUNC] = {
-        {"", "", "", ""},
-        {"", "", "", ""},
-        {"", "", "", ""},
-        {"", "", "", ""},
-        {"", "BASE ", "", "R/FN "}
+    [_NUM] = {
+        {"    ", "     ", "    ", "     "},
+        {"    ", "     ", "    ", "     "},
+        {"    ", "     ", "    ", "     "},
+        {"    ", "     ", "    ", "     "},
+        {"    ", "BASE ", "    ", "R/FN "}
     },
 
-    [_FUNC2] = {
-        {"", "", "", ""},
-        {"", "", "", "BL+  "},
-        {"", "", "", "BL-  "},
-        {"", "", "", ""},
-        {"", "", "", ""}
+    [_FUNC] = {
+        {"    ", "    ", "    ", "     "},
+        {"    ", "    ", "    ", "BL+  "},
+        {"    ", "    ", "    ", "BL-  "},
+        {"    ", "    ", "    ", "     "},
+        {"    ", "    ", "    ", "     "}
     }
 };
 
-/* array of keycode aliases in PROGMEM? it's FUCKING massive, though*/
+/* array of keycode aliases in PROGMEM. it's FUCKING massive, though*/
 #define NUMBER_OF_ALIASES 0xF0
 const char keycode_aliases[NUMBER_OF_ALIASES][BUFFER_SIZE] PROGMEM = {
     "     ",
@@ -322,7 +322,6 @@ const char keycode_aliases[NUMBER_OF_ALIASES][BUFFER_SIZE] PROGMEM = {
     "RGUI",
 };
 
-
 void matrix_init_user(void) {}
 
 void matrix_scan_user(void) {}
@@ -333,12 +332,12 @@ void led_set_user(uint8_t usb_led) {
 
 uint32_t layer_state_set_user(uint32_t state) {
     switch (biton32(state)) {
-        case _FUNC:
+        case _NUM:
             writePinLow(B0);
             writePinHigh(D5);
             break;
 
-        case _FUNC2:
+        case _FUNC:
             writePinHigh(B0);
             writePinLow(D5);
             break;
@@ -354,14 +353,14 @@ uint32_t layer_state_set_user(uint32_t state) {
 #ifdef OLED_DRIVER_ENABLE
 void oled_task_user(void) {
     switch (get_highest_layer(layer_state)) {
-        case _FUNC:
+        case _NUM:
             oled_write_ln_P(PSTR("Layer: NUM"), false);
-            oled_print_keymap_aliases(_FUNC);
+            oled_print_keymap_aliases(_NUM);
             break;
 
-        case _FUNC2:
+        case _FUNC:
             oled_write_ln_P(PSTR("Layer: FN"), false);
-            oled_print_keymap_aliases(_FUNC2);
+            oled_print_keymap_aliases(_FUNC);
             break;
 
         case _BASE:
